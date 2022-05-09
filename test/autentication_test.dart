ac484d3a201008242f32b6263421c57e956065c9
Anullos <anullos988@gmail.com>
2022-05-09 18:30:07 +0200

    [MEMORY] - progress

---
+++ test/autentication_test.dart
@@ -0,0 +1,18 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('initial state is LoginInitial', () {
+    expect(1, 1);
+    expect(1, 1);
+  });
+
+  group('login', () {
+    test('username and password are corect', () async {
+      expect(1, 1);
+    });
+
+    test('username and password are not corect', () async {
+      expect(1, 1);
+    });
+  });
+}
---
+++ test/products_test.dart
@@ -0,0 +1,21 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('initial state is productsController', () {
+    expect(1, 1);
+  });
+
+  group('products', () {
+    test('get products', () async {
+      expect(1, 1);
+    });
+
+    test('add product', () async {
+      expect(1, 1);
+    });
+
+    test('delete product', () async {
+      expect(1, 1);
+    });
+  });
+}
